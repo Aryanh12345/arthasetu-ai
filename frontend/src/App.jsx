b77824c133e347a68f2ff046de4e92f97c1d5b99
Aryanh12345 <hapaliyaaryan24@gmail.com>
2026-07-10 19:21:47 +0530

    Sprint 3: Add dashboard service and financial behaviour module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Register from "./pages/Register";
+import FinancialBehaviour from "./pages/FinancialBehaviour";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/financial" element={<FinancialBehaviour />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
